5402b9cdf97fccf9231ac8e45d0b6864fefa7b0c
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-03-28 21:00:58 -0700

    forms and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -20,5 +20,31 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 
 end
